403191061ba5f0c1365d9fcba53247d20e1afc49
Iryna <irynalatynina@gmail.com>
2025-07-07 20:41:21 +0100

    Add footer layout with social media icons and styles
    New footer section implemented in SCSS and CSS with text, links, and social media icons for Facebook, Twitter, and Instagram. Added corresponding SVG assets and updated JavaScript for carousel and accordion functionality.

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$('.owl-carousel').owlCarousel({
+    autoplay:true,
+    autoplayTimeout: 1500,
+    autoplaySpeed: 1000,
+    loop: true,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        1100:{
+            items:1
+        }
+    }
+
+})
+
+$( function() {
+    $( "#accordion" ).accordion();
+} );
+
